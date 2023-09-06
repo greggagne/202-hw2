d9ee243d6564455fd161981331658e624d4371b0
greggagne <ggagne@westminsteru.edu>
2023-09-06 15:56:37 -0600

    Initial commit.

---
+++ src/ArrayListBag.java
@@ -0,0 +1,28 @@
+public class ArrayListBag implements Bag
+{
+
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public void addAll(Object[] items) {
+
+    }
+
+    @Override
+    public boolean contains(Object item) {
+        return false;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean remove(Object item) {
+        return false;
+    }
+}
